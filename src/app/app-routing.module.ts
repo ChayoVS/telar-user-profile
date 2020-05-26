c67442d9002ecd8f0d791584f9647ba6e9a57a19
ChayoVS <paradisekiss-yaoi@hotmail.com>
2020-05-26 14:34:15 -0500

    Reacomodé el menú de navegación,

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NavComponent } from './component/nav/nav.component';
+
 import { PerfilComponent } from './component/perfil/perfil.component';
 
 const routes: Routes = [
-  {path: '', component:NavComponent},
-  {path:'',component:PerfilComponent}
+  {path: '', component: PerfilComponent},
+  {path:'perfil-usuario', component:PerfilComponent}
 
 ];
 
 @NgModule({
   declarations:[
-    NavComponent,
     PerfilComponent
   ],
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'telar-profile-user';
+
+  ngOnInit(): void {
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  }
 }
