e24e08acfc41cb8f7e544c1c1e9c9d2279b935fb
ChayoVS <paradisekiss-yaoi@hotmail.com>
2020-05-26 10:29:43 -0500

    quité una línea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import * as $ from 'jquery'
+
 @NgModule({
   declarations: [
     AppComponent
