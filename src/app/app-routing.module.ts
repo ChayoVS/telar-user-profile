b329e213d25296d87fe486028e47111ecca30ed3
ChayoVS <paradisekiss-yaoi@hotmail.com>
2020-05-27 09:05:49 -0500

    Moví el outlet router para las vistas, creé vista home para comprobarlo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PerfilComponent } from './component/perfil/perfil.component';
+import { HomeComponent } from './component/home/home.component';
 
 const routes: Routes = [
   {path: '', component: PerfilComponent},
-  {path:'perfil-usuario', component:PerfilComponent}
+  {path: 'perfil-usuario', component: PerfilComponent},
+  {path: 'home', component: HomeComponent }
 
 ];
 
